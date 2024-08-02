f2a67e9a646cbe3cb36871d7a72992b5b9cbd0d3
Ayrlon Vilarim <ayrlonvilarim@joystreet.com.br>
2024-08-02 20:59:05 -0300

    ajuste de nome de usecase

--- src/modules/user/infra/user.module.ts
+++ src/modules/user/infra/user.module.ts
@@ -3,7 +3,7 @@ import { UserController } from './controller/user.controller';
 import { UserRepository } from '@modules/user/application/repositories/user.repository';
 import FakeUserRepository from '@modules/user/infra/repositories/fakeUser.repository';
 import CreateUserUseCase from '@modules/user/application/useCases/createUser/createUser.useCase';
-import GetUserUseCaseByUsername from '@modules/user/application/useCases/getUserByUsername/getUserByUsername.useCase';
+import GetUserByUsernameUseCase from '@modules/user/application/useCases/getUserByUsername/getUserByUsername.useCase';
 import GetUserByIdUseCase from '@modules/user/application/useCases/getUserById/getUserById.useCase';
 import UserUseCases from '@modules/user/application/useCases/user.useCases';
 
@@ -18,9 +18,9 @@ import UserUseCases from '@modules/user/application/useCases/user.useCases';
       inject: [UserRepository],
     },
     {
-      provide: GetUserUseCaseByUsername,
+      provide: GetUserByUsernameUseCase,
       useFactory: (userRepository: UserRepository) => {
-        return new GetUserUseCaseByUsername(userRepository);
+        return new GetUserByUsernameUseCase(userRepository);
       },
       inject: [UserRepository],
     },
@@ -34,12 +34,12 @@ import UserUseCases from '@modules/user/application/useCases/user.useCases';
     {
       provide: UserUseCases,
       useFactory: (
-        getUserUseCase: GetUserUseCaseByUsername,
+        getUserUseCase: GetUserByUsernameUseCase,
         createUserUseCase: CreateUserUseCase,
         getUserByIdUseCase: GetUserByIdUseCase,
       ) =>
         new UserUseCases(getUserUseCase, createUserUseCase, getUserByIdUseCase),
-      inject: [GetUserUseCaseByUsername, CreateUserUseCase, GetUserByIdUseCase],
+      inject: [GetUserByUsernameUseCase, CreateUserUseCase, GetUserByIdUseCase],
     },
   ],
   exports: [],
