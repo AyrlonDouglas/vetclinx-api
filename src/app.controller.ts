034b18f730a0aeff177f06adcb7b0d7e65660d25
Ayrlon <ayr.l@hotmail.com>
2025-01-27 18:16:33 -0300

    ajustes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
+import { ApiPresenter } from '@common/infra/Api.presenter';
 import { Controller, Get } from '@nestjs/common';
 
 @Controller('app')
 export class AppController {
   @Get('health')
   health() {
-    return { ok: true };
+    return new ApiPresenter({
+      result: { healthy: true },
+      message: 'A Api está saudável.',
+    });
   }
 }
